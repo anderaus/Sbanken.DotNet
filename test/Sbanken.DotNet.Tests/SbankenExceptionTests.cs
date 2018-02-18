13c26fdc81f1f63445c94d668a22e9159421bf04
Anders Austad <anders.austad@novanet.no>
2018-02-18 20:31:40 +0100

    ✅ Add test project and initial tests

---
+++ test/Sbanken.DotNet.Tests/SbankenExceptionTests.cs
@@ -0,0 +1,28 @@
+﻿using Sbanken.DotNet.Exceptions;
+using Xunit;
+
+namespace Sbanken.DotNet.Tests
+{
+    public class SbankenExceptionTests
+    {
+        [Fact]
+        public void ToStringOutputsMessage()
+        {
+            var exception = new SbankenException("This is the message");
+
+            var output = exception.ToString();
+
+            Assert.Equal("Sbanken.DotNet.Exceptions.SbankenException: This is the message", output);
+        }
+
+        [Fact]
+        public void ToStringOutputsErrorType()
+        {
+            var exception = new SbankenException("This is the message", "This is the errortype");
+
+            var output = exception.ToString();
+
+            Assert.Equal("Sbanken.DotNet.Exceptions.SbankenException: This is the message, ErrorType: This is the errortype", output);
+        }
+    }
+}
